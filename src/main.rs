9273161f771adf51b6e6619517a0c6140aa0801f
skirit <skirit@dump.cz>
2019-09-01 03:38:31 +0200

    add threads

--- src/main.rs
+++ src/main.rs
@@ -5,43 +5,60 @@ use std::io::{BufReader, BufRead};
 
 static ADDR: &str = "irc.freenode.org:6667";
 static CHAN: &str = "#testbot32";
-static NICK: &str = "osw-switch";
+static NICK: &str = "osw-bot";
 
 fn main() -> std::io::Result<()> {
 	loop{
 		let mut s = TcpStream::connect(ADDR)?;
 		s.set_read_timeout(Some(time::Duration::new(360, 0)))?;
 		let mut r = BufReader::new(s.try_clone()?);
+		let mut topic = String::new();
 
 		s.write(format!("NICK {}\n", NICK).as_ref())?;
 		s.write(format!("USER {} 0 * :test bot\n", NICK).as_ref())?;
 		s.write(format!("JOIN {}\n", CHAN).as_ref())?;
+		
+		let ss = s.try_clone()?;
+		let top = topic.clone();
+
+		thread::spawn(move || {
+    		checksw(&top, ss);
+    	//	checksw(ss);
+		});
 
 		loop{
 			let mut data = String::new();
 			match r.read_line(&mut data) {
 				Ok(0) => { thread::sleep(time::Duration::new(180, 0)); break; }
-				Ok(_) => { eval(data.trim_end().to_string(), s.try_clone()?)?; }
-				Err(_) => { thread::sleep(time::Duration::new(180, 0)); break; }
+				Err(_)=> { thread::sleep(time::Duration::new(180, 0)); break; }
+				Ok(_) => { eval(data.trim_end().to_string(), &mut topic, s.try_clone()?)?; }
 			}
 		}
 	}
 }
 
-fn eval(mut data: String, mut s: TcpStream) -> std::io::Result<()> {
+fn checksw(topic: &String, mut _s: TcpStream){
+	loop{
+		thread::sleep(time::Duration::new(5, 0));
+		println!("{:#?}", topic);
+	}
+}
+
+fn eval(mut data: String, topic: &mut String, mut s: TcpStream) 
+-> std::io::Result<()> {
 	println!("{:#?}", data);
 
 	if data.starts_with("PING :") {
-		let (_,last) = data.split_at(6);
-		s.write(format!("PONG :{}\n", last).as_ref())?;
-		println!("{:#?}", format!("PONG :{}\n", last));
+		s.write(format!("PONG :{}\n", data.trim_start_matches("PING :")).as_ref())?;
+		println!("{:#?}", format!("PONG :{}\n", data.trim_start_matches("PING :")));
 	}
 	else{
 		data.remove(0);
 		let (_,last) = data.split_at(data.find(' ').unwrap() + 1);
 		if last.starts_with("332 ") || last.starts_with("TOPIC ") {
 			let (_,last) = data.split_at(data.find(':').unwrap() + 1);
-			println!("TOPIC JE: {:#?}", last);
+			topic.clear();
+			topic.push_str(last);
 		}
 		if last.starts_with("PRIVMSG ") {
 			let (_,last) = data.split_at(data.find(':').unwrap() + 1);
